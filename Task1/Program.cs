074eec5cf454e960514e07d01962b23101450bf3
Ilya Silkin <zerolar@gmail.com>
2021-07-16 21:34:48 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Читаем файл по указанному пути
+            Console.WriteLine(@"Введите директорию файла (пример - D:\TestFolder\Text1.txt): ");
+            string dirName = Console.ReadLine();
+
+            while (!File.Exists(dirName))
+            {
+                Console.WriteLine("Файл не найден, попробуйте снова.\n");
+                dirName = Console.ReadLine();
+            }
+
+            var text = File.ReadAllText(dirName);
+
+            // Преобразуем текст в массив символов
+            var characters = text.ToCharArray();
+
+            //Создаем простой список List
+            var simpleList = new List<char>(characters);
+
+            //Создаем связанный список LinkedList
+            var linkedList = new LinkedList<char>(characters);
+
+            //Проверяем производительность вставки в простой список
+            var stopWatch = Stopwatch.StartNew();
+
+            simpleList.Add(Convert.ToChar("j"));
+            var simpleListTime = stopWatch.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"Время вставки для простого списка {simpleListTime} мс");
+
+            //Провереям производительность вставки в связанный список
+            stopWatch = Stopwatch.StartNew();
+
+            linkedList.AddLast(Convert.ToChar("j"));
+            var linkedListTime = stopWatch.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"Время вставки для связанного списка {linkedListTime} мс");
+
+            //Сравниваем производительности
+            if (simpleListTime > linkedListTime)
+            {
+                Console.WriteLine($"Производительность вставки в связанный список больше в {Math.Round((simpleListTime / linkedListTime), 1)} раз(а)");
+            }
+            else
+            {
+                Console.WriteLine($"Производительность вставки в простой список больше в {Math.Round((linkedListTime / simpleListTime), 1)} раз(а)");
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Читаем файл по указанному пути
+            Console.WriteLine(@"Введите директорию файла (пример - D:\TestFolder\Text1.txt): ");
+            string dirName = Console.ReadLine();
+
+            while (!File.Exists(dirName))
+            {
+                Console.WriteLine("Файл не найден, попробуйте снова.\n");
+                dirName = Console.ReadLine();
+            }
+
+            var text = File.ReadAllText(dirName);
+
+
+            // Разбиваем текст на отдельные слова без знаков пунктуации
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            var delimiters = new char[] { ' ', '\n', '\r' };
+            var words = noPunctuationText.ToLower().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // Группируем слова по частоте встречаемости
+            var wordGroups1 = from a in words
+                              group a by a into b
+                              select new { b.Key, Count = b.Count() } into c
+                              orderby c.Count descending
+                              select c;
+
+            var wordGroups2 = words.GroupBy(a => a).Select(b => new { b.Key, Count = b.Count() }).OrderByDescending(c => c.Count);
+
+            // Выводим 10 самых часто встречающихся слов
+            Console.WriteLine("10 самых часто встречающихся слов: ");
+            Console.WriteLine("(слово - количество раз)");
+            foreach (var word in wordGroups1.Take(10))
+            {
+                Console.WriteLine($"{word.Key} - {word.Count}");
+            }
+        }
+    }
+}
